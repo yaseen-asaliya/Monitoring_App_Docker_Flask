98ad5f58393c95a820c624a24f1c2fa7f8375fd7
Yaseen Asaliya <yaseen.asaliya22@gmail.com>
2023-02-06 12:55:31 +0200

    modify apache

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -1,18 +1,26 @@
 FROM python:3.9-slim-buster
 
+# Apache2 package with it's dependencies libapr1 and libaprutil1 
 # sysstat to run "mpstat" command package
 # procps to run "free -m" command package
-RUN apt-get update && apt-get -y install cron python3 python3-pip sysstat procps apache2
+RUN apt-get update && apt-get -y install apache2 libapr1 libaprutil1 sysstat procps
+
+RUN apt-get update && apt-get -y install python3 python3-pip
 RUN pip3 install flask requests
 
 WORKDIR /app
 
+# set the ServerName directive in Apache's configuration
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+# Copy the cron job, Python files, and the HTML file
 COPY crontab /etc/cron.d/crontab
 COPY *py /app/
 COPY index.html /var/www/html/
 
+# Give proper permissions to the cron job
 RUN chmod 0644 /etc/cron.d/crontab
 RUN crontab /etc/cron.d/crontab
 
-ENTRYPOINT cron; FLASK_APP=/app/app.py flask run --host=0.0.0.0; /usr/sbin/apachectl -D FOREGROUND
-
+# Start Apache2 and the Flask app
+ENTRYPOINT service apache2 start && cron && FLASK_APP=/app/app.py flask run --host=0.0.0.0
--- set_html.py
+++ set_html.py
@@ -8,8 +8,9 @@ def get_container_ip():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
-    
+
 def get_data_from_url(endpoint):
+    container_ip = get_container_ip()
     base_url = f"http://{container_ip}:5000"
     response = requests.get(base_url + endpoint)
     result = []
@@ -17,7 +18,7 @@ def get_data_from_url(endpoint):
         print("The request was successful. The response content:")
         result = response.content
     else:
-        print("The request failed with status code:", response.status_code) 
+        print("The request failed with status code:", response.status_code)
     return result
 
 def get_css():
@@ -47,7 +48,7 @@ def get_css():
         background-color: #dddddd;
         font-weight: bold;
         }
-        
+
          .buttons {
         display: flex;
         justify-content: left;
@@ -56,7 +57,7 @@ def get_css():
       }
 
       .button {
-        background-color: #938f8f; 
+        background-color: #938f8f;
         border: none;
         color: white;
         padding: 15px 32px;
@@ -68,7 +69,7 @@ def get_css():
         cursor: pointer;
         border-radius: 5px;
       }
-      
+
         </style>
     """
 
@@ -78,7 +79,7 @@ def get_table_header(arr):
         result += f"<th>{item}</th>\n"
     result += "</tr>\n"
     return result
-    
+
 def get_buttons():
     return """
     <div class="buttons">
@@ -90,7 +91,7 @@ def get_buttons():
 
 def get_tabel(data):
     result = ""
-    for item in data: 
+    for item in data:
         result += "<tr>\n"
         for element in item:
             result += f"<th>{item[element]}</th>\n"
@@ -103,39 +104,39 @@ def create_html_file(file_name, table_name, header, data):
         file.write(get_css())
         file.write("</head><body>\n")
         file.write(get_buttons())
-        
-        file.write(f"<div>\n<h2>Current {table_name} Usage</h2><br><table>") 
+
+        file.write(f"<div>\n<h2>Current {table_name} Usage</h2><br><table>")
         file.write(get_table_header(header))
         file.write(get_tabel(data['current']))
-        
-        file.write(f"</table></div>\n</div>\n<h2>{table_name} Usage for last 24h</h2><br><table>")   
+
+        file.write(f"</table></div>\n</div>\n<h2>{table_name} Usage for last 24h</h2><br><table>")
         header.insert(0,"Time")
         file.write(get_table_header(header))
         file.write(get_tabel(data['usage_for_last_24h']))
         file.write("</table>\n</div></html></body>")
-        
+
 def initilaize_html_files():
     disk_header = ["Filesystem","Size","Used","Avail","Use","Mounted on"]
     memory_header = ["Name","Total","Used","Free","Shared","Buff/cache","Available"]
     cpu_header = ["CPU","usr%","nice%","sys%","iowait%","irq%","soft%","steal%","guest%","gnice%","idle%"]
-    
+
     create_html_file("disk.html","Disks",disk_header,disk)
     create_html_file("memory.html","Memorys",memory_header,memory)
     create_html_file("cpu.html","CPU",cpu_header,cpu)
 
 def copy_file_to_apache_dir():
     files = ["disk","memory","cpu"]
-    
+
     for file_name in files:
-        src_dir = f"/app/{file_name}.html"
+        src_dir = f"{file_name}.html"
         dst_dir = f"/var/www/html/{file_name}.html"
         shutil.move(src_dir, dst_dir)
-        
+
+
 if __name__ == "__main__":
-    container_ip = get_container_ip()
     disk = json.loads(get_data_from_url("/disk"))
     memory = json.loads(get_data_from_url("/memory"))
     cpu = json.loads(get_data_from_url("/cpu"))
-    
+
     initilaize_html_files()
     copy_file_to_apache_dir()
